b78a56b996caee6bba900c056dd8f97afd07739e
pasindu-weerasingha <pasinduhas@gmail.com>
2023-08-26 20:09:49 +0530

    Added WebSecurity Configuration

---
+++ src/main/java/com/codelantic/configuration/WebSecurityConfiguration.java
@@ -0,0 +1,15 @@
+package com.codelantic.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+
+}
